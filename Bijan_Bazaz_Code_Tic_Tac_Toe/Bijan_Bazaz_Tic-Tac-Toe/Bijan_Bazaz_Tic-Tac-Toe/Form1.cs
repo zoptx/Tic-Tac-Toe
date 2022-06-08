932c4dba11e89ecc5c764a20344e19c5ddd63914
zoptx <107120645+zoptx@users.noreply.github.com>
2022-06-08 09:22:31 -0400

    Add files via upload

---
+++ Bijan_Bazaz_Code_Tic_Tac_Toe/Bijan_Bazaz_Tic-Tac-Toe/Bijan_Bazaz_Tic-Tac-Toe/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bijan_Bazaz_Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true;// true = X turn; false = Y turn
+        int turn_count = 0; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Par Bijan", "Tic Tac Toe About");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
